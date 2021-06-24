16ca67f52897a75c43f36e848ebdb987a4450982
A308895 <hladera@santandertecnologia.com.ar>
2021-06-23 23:08:33 -0300

    correccon

--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -2,53 +2,49 @@ package rest_errors
 
 import (
 	"errors"
-	"net/http"
+	"fmt"
 )
 
-type RestErr struct {
-	Message string        `json:"message"`
-	Status  int           `json:"status"`
-	Error   string        `json:"error"`
-	Causes  []interface{} `json:"causes"`
+type RestErr interface {
+	Message() string
+	Status() int
+	Error() string
+	Causes() []interface{}
+}
+
+type restErr struct {
+	ErrMessage string        `json:"message"`
+	ErrStatus  int           `json:"status"`
+	ErrError   string        `json:"error"`
+	ErrCauses  []interface{} `json:"causes"`
+}
+
+func (e restErr) Error() string {
+	return fmt.Sprintf("message: %s - status: %d - error %s - causes: %v",
+		e.ErrMessage, e.ErrError, e.ErrCauses)
+}
+
+func (e restErr) Status() int {
+	return e.ErrStatus
+}
+
+func (e restErr) Message() string {
+	return e.ErrMessage
+}
+
+func (e restErr) Causes() []interface{} {
+	return e.ErrCauses
+}
+
+func NewRestError(message string, status int, err string, causes []interface{}) RestErr {
+	return restErr{
+		ErrMessage: message,
+		ErrStatus:  status,
+		ErrError:   err,
+		ErrCauses:  causes,
+	}
 }
 
 func NewError(msg string) error {
 	return errors.New(msg)
-}
-
-func NewBadRequestError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusBadRequest,
-		Error:   "bad_request",
-	}
-}
-
-func NewNotFoundError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusNotFound,
-		Error:   "not_found",
-	}
-}
-
-func NewInternalServerError(message string, err error) *RestErr {
-	result := &RestErr{
-		Message: message,
-		Status:  http.StatusInternalServerError,
-		Error:   "internal_server_error",
-	}
-	if err != nil {
-		result.Causes = append(result.Causes, err.Error())
-	}
-	return result
-}
-
-func NewRestError(message string, status int64, cod_error string) *RestErr {
-	result := &RestErr{
-		Message: message,
-		Status:  http.StatusInternalServerError,
-		Error:   cod_error,
-	}
-	return result
 }
